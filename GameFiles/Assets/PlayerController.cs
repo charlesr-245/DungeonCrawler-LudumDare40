19998372d8b9e91bee4e3f7d437df18e2cbc0423
Cole <coolcole0225@gmail.com>
2017-12-02 08:53:11 -0800

    Finished the PlayerController, started the PlayerAttack

---
+++ GameFiles/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿  using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Rigidbody rb;
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	void Movement()
+    {
+        Vector3 Direction;
+        Direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * speed;
+        rb.velocity = Direction;
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
+        
+        
+    }
+    
+	void FixedUpdate () {
+        Movement();
+    
+
+        }
+	}
+}
