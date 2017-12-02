c36deeafdf0d29f99f75917b5fcaa057f332f679
Charles <charlesreverand.01@gmail.com>
2017-12-02 14:02:07 -0500

    Commit for Compatability
    Readying for merge

--- GameFiles/Assets/PlayerController.cs
+++ GameFiles/Assets/PlayerController.cs
@@ -5,24 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public Rigidbody rb;
     public float speed;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
+
 	void Movement()
     {
-        Vector3 Direction;
-        Direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * speed;
-        rb.velocity = Direction;
+        Vector3 direction;
+        direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * speed;
+        rb.velocity = direction;
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
-        
-        
     }
     
 	void FixedUpdate () {
         Movement();
-    
-
-        }
-	}
+    }
 }
