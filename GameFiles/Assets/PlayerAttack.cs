7606daabfb9a1074cf3298a8db539572cd8079cf
Cole <coolcole0225@gmail.com>
2017-12-03 10:45:30 -0800

    nothing much

--- GameFiles/Assets/PlayerAttack.cs
+++ GameFiles/Assets/PlayerAttack.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour {
     void MeleeAttack()
     {
-        float distance = Vector3.Distance(TargetJoint2D.transform.position, transform.position);
-
-        if (distance < 1f)
-        {
-            EnemyHealth EHP=
+       
         }
+       
     }
-	void FixedUpdate () {
-        if (Input.GetButton("Fire1"))
-        {
-            MeleeAttack();
-        }
-	}
+    void FixedUpdate()
+    {
+    if(Input.GetButton("Fire 1"))
+    {
+
+    }
+    }
 }
+
